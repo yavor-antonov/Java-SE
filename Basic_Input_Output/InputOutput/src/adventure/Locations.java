2d37e838757c69c64621650a535268da8ce355be
yavor-antonov <108179274+yavor-antonov@users.noreply.github.com>
2022-08-29 23:19:48 +0300

    Updated Basic I/O tasks

--- Basic_Input_Output/InputOutput/src/adventure/Locations.java
+++ Basic_Input_Output/InputOutput/src/adventure/Locations.java
@@ -1,62 +1,114 @@
 package adventure;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) throws IOException{
-        FileWriter locFile = null;
-        try {
-            locFile = new FileWriter("locations.txt");
+        try (FileWriter locFile = new FileWriter("locations.txt");
+             FileWriter dirFile = new FileWriter("directions.txt")){
             for (Location location : locations.values()){
                 locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-                throw new IOException("test exception thrown while writing");
-            }
-        } finally {
-            System.out.println("In finally block");
-            if (locFile != null) {
-                System.out.println("Attempting to close locfile");
-                locFile.close();
+                for (String direction : location.getExits().keySet()){
+                    dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                }
             }
         }
+
+//        FileWriter locFile = null;
+//        try {
+//            locFile = new FileWriter("locations.txt");
+//            for (Location location : locations.values()){
+//                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+//                throw new IOException("test exception thrown while writing");
+//            }
+//        } finally {
+//            System.out.println("In finally block");
+//            if (locFile != null) {
+//                System.out.println("Attempting to close locfile");
+//                locFile.close();
+//            }
+//        }
 
     }
 
     static {
-        Map<String,Integer> tempExit = new HashMap<String,Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(new FileReader("locations.txt"));
+            scanner.useDelimiter(",");
+            while(scanner.hasNextLine()){
+                int loc = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String description = scanner.nextLine();
+                System.out.println("Imported loc:" + loc + ": " + description);
+                Map<String, Integer> tempExit = new HashMap<>();
+                locations.put(loc, new Location(loc,description,tempExit));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (scanner!= null){
+                scanner.close();
+            }
+        }
 
-        tempExit = new HashMap<String,Integer>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+        // Now read the exits
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+            scanner.useDelimiter(",");
+            while(scanner.hasNextLine()){
+                int loc = scanner.nextInt();
+                scanner.skip(scanner.delimiter());
+                String direction = scanner.next();
+                scanner.skip(scanner.delimiter());
+                String dest = scanner.nextLine();
+                int destination = Integer.parseInt(dest);
+                System.out.println(loc + ": " + direction + ": " + destination);
+                Location location = locations.get(loc);
+                location.addExit(direction,destination);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (scanner != null){
+                scanner.close();
+            }
+        }
 
-        tempExit = new HashMap<String,Integer>();
-        tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
-
-        tempExit = new HashMap<String,Integer>();
-        tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building a well house for a small spring",tempExit));
-
-        tempExit = new HashMap<String,Integer>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
-
-
-        tempExit = new HashMap<String,Integer>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest",tempExit));
+//        Map<String,Integer> tempExit = new HashMap<String,Integer>();
+//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+//
+//        tempExit = new HashMap<String,Integer>();
+//        tempExit.put("W", 2);
+//        tempExit.put("E", 3);
+//        tempExit.put("S", 4);
+//        tempExit.put("N", 5);
+//        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+//
+//        tempExit = new HashMap<String,Integer>();
+//        tempExit.put("N", 5);
+//        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+//
+//        tempExit = new HashMap<String,Integer>();
+//        tempExit.put("W", 1);
+//        locations.put(3, new Location(3, "You are inside a building a well house for a small spring",tempExit));
+//
+//        tempExit = new HashMap<String,Integer>();
+//        tempExit.put("N", 1);
+//        tempExit.put("W", 2);
+//        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+//
+//
+//        tempExit = new HashMap<String,Integer>();
+//        tempExit.put("S", 1);
+//        tempExit.put("W", 2);
+//        locations.put(5, new Location(5, "You are in the forest",tempExit));
 
     }
 
